c8026888a0b806b8fdb1ed656b486777e6593b9f
Chuck Dries <chuck@chuckdries.com>
2022-03-09 08:38:02 -0700

    run build

--- dist/src.f69400ca.js
+++ dist/src.f69400ca.js
@@ -66812,7 +66812,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65400" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59102" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
